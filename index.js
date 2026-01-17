27decc0846adcfc74551082a5b398ce2252a79f3
Abu Ameen <113390687+Abu-Ameenudeen@users.noreply.github.com>
2026-01-17 19:07:58 +0530

    Add dynamic post update to the body
    Added:
        - Added template string to update the content of the body to render
        all the post to the body

--- index.js
+++ index.js
@@ -28,3 +28,43 @@ const posts = [
     }
 ]
 
+const postsContainer = document.getElementById("posts-container")
+
+postsContainer.innerHTML = ""
+
+for (let post of posts) {
+  postsContainer.innerHTML += `
+    <section class="post">
+      <div class="post-header">
+        <img class="feed-avatar" src="${post.avatar}" alt="poster's avatar">
+        <div class="name-location">
+          <div>
+            <span class="name text-bold">${post.name}</span>
+          </div>
+          <span class="location">${post.location}</span>
+        </div>
+      </div>
+
+      <img class="feed-post" src="${post.post}" alt="post image">
+
+      <div class="post-reaction-section">
+        <img class="post-reaction-btn like-btn" src="images/icon-heart.png" alt="like button">
+        <img class="post-reaction-btn cmt-btn" src="images/icon-comment.png" alt="comment button">
+        <img class="post-reaction-btn dm-btn" src="images/icon-dm.png" alt="dm button">
+      </div>
+
+      <div>
+        <h2 class="likes-count">
+          <span class="text-bold">${post.likes}</span> likes
+        </h2>
+      </div>
+
+      <div>
+        <h3 class="feed-caption">
+          <span class="text-bold">${post.username}</span>
+          <span class="caption-text">${post.comment}</span>
+        </h3>
+      </div>
+    </section>
+  `
+}
